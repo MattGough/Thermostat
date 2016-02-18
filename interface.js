2fdb3b1143c2a557b4b82d98becb0ebb49a11062
Matt <mattpgough@gmail.com>
2016-02-18 19:26:32 +0000

    Linked up buttons with jQuery, added css to manage colours.

--- interface.js
+++ interface.js
@@ -1,9 +1,35 @@
 $(document).ready(function() {
   var thermostat = new Thermostat();
-  $('#temperature').text(thermostat.temperature);
+  updateTemperature();
 
-$('#Increase-temperature').click(function() {
-  thermostat.increaseTemperature();
-  $('#temperature').text(thermostat.temperature);
- });
+  $('#Increase_temp').click(function() {
+   thermostat.increaseTemperature();
+   updateTemperature();
+  });
+
+  $('#Decrease_temp').click(function() {
+   thermostat.decreaseTemperature();
+   updateTemperature();
+  });
+
+  $('#Reset_Temp').click(function() {
+   thermostat.resetTemperature();
+   updateTemperature();
+  });
+
+  $('#PSM_ON').click(function() {
+   thermostat.switchPowerSavingModeOn();
+   $('#power-saving-status').text('on');
+  });
+
+  $('#PSM_OFF').click(function() {
+   thermostat.switchPowerSavingModeOff();
+   $('#power-saving-status').text('off');
+  });
+
+  function updateTemperature(){
+   $('#temperature').text(thermostat.temperature);
+   $('#temperature').attr('class', thermostat.displayColour());
+  }
+
 });
