519c0e0bc701f75174db57efffa2acdc00621ef3
Matt <mattpgough@gmail.com>
2016-02-18 21:03:37 +0000

    Added weather API to show current temp in different cities.

--- interface.js
+++ interface.js
@@ -28,8 +28,31 @@ $(document).ready(function() {
   });
 
   function updateTemperature(){
-   $('#temperature').text(thermostat.temperature);
+   $('#temperature').text( thermostat.temperature);
    $('#temperature').attr('class', thermostat.displayColour());
   }
 
+  $('#current-city').change(function() {
+    var city = $('#current-city').val();
+  $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=44db6a862fba0b067b1930da0d769e98&units=metric', function(data) {
+  $('#current-temperature').text(data.main.temp);
+  })
+ })
+
 });
+
+// function displayWeather(city) {
+// var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city;
+// var token = '&appid=44db6a862fba0b067b1930da0d769e98';
+// var units = '&units=metric';
+// $.get(url + token + units, function(data) {
+//   $('#current-temperature').text(data.main.temp);
+// })
+//
+// displayWeather('London');
+//
+// $('#select-city').submit(function(event) {
+//  event.preventDefault();
+//  var city = $('#current-city').val();
+//  displayWeather(city);
+// })
